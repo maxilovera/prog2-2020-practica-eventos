be1402227dab4545b18f58481bb0fc1bfd6fc537
maxilovera <maximilianolovera@gmail.com>
2021-05-14 19:13:36 -0300

    prueba

--- ServiciosWeb/Service1.svc.cs
+++ ServiciosWeb/Service1.svc.cs
@@ -11,7 +11,7 @@ namespace ServiciosWeb
 {
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
     // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
-    public class Service1 : IService1
+    public class Service1 : IServicioGenerico
     {        
         public List<ClienteParaCompartir> GetClientes(string nombre)
         {
@@ -21,7 +21,7 @@ namespace ServiciosWeb
             {
                 Nombre = x.Nombre,
                 Apellido = "No tiene"
-            }).ToList();
+            }).ToArray();
         }
     }
 
